56b55ea6405d0b35393b998846f5d072df649eb9
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-08-13 21:20:52 +0300

    wip testRepayDebt fork

--- test/BaseTestFork.sol
+++ test/BaseTestFork.sol
@@ -4,5 +4,11 @@ pragma solidity 0.8.19;
 import {BaseTest} from "./BaseTest.sol";
 
 abstract contract BaseTestFork is BaseTest {
-    function _testSetup() internal override {}
+    string internal MONAD_TESTNET_RPC_URL = "https://testnet-rpc.monad.xyz";
+    uint256 internal MONAD_TESTNET_BLOCK_NUMBER = 30_591_458;
+
+    function _testSetup() internal override {
+        uint256 monadTestnetFork = vm.createFork(MONAD_TESTNET_RPC_URL, MONAD_TESTNET_BLOCK_NUMBER);
+        vm.selectFork(monadTestnetFork);
+    }
 }
--- test/fork/UserVaultFork.t.sol
+++ test/fork/UserVaultFork.t.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.19;
+
+import "../BaseTestFork.sol";
+import {IPoolAddressesProviderRegistry} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProviderRegistry.sol";
+import "forge-std/console.sol";
+
+contract UserVaultTest is BaseTestFork {
+    address poolDataProviderAddress = 0x2F7ae2EebE5Dd10BfB13f3fB2956C7b7FFD60A5F;
+
+    function testRepayDebt() public {
+        // arrange
+        IPoolAddressesProviderRegistry papr = IPoolAddressesProviderRegistry(poolDataProviderAddress);
+        address[] memory addresses = papr.getAddressesProvidersList();
+
+        address pool1 = addresses[0];
+
+        emit log_named_address("pool address", pool1);
+    }
+}
