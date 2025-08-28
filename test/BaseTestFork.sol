05be61b954a9d5ba88e539dd8c05b6a2b43e9d27
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-08-29 02:54:45 +0300

    disabled fork tests - they do not work on monad

--- test/BaseTestFork.sol
+++ test/BaseTestFork.sol
@@ -5,10 +5,4 @@ import {BaseTest} from "./BaseTest.sol";
 
 abstract contract BaseTestFork is BaseTest {
     string internal MONAD_TESTNET_RPC_URL = "https://testnet-rpc.monad.xyz";
-    uint256 internal MONAD_TESTNET_BLOCK_NUMBER = 30_591_458;
-
-    function _testSetup() internal override {
-        uint256 monadTestnetFork = vm.createFork(MONAD_TESTNET_RPC_URL, MONAD_TESTNET_BLOCK_NUMBER);
-        vm.selectFork(monadTestnetFork);
-    }
 }
--- test/fork/UserVaultFork.t.sol
+++ test/fork/UserVaultFork.t.sol
@@ -2,19 +2,26 @@
 pragma solidity 0.8.19;
 
 import "../BaseTestFork.sol";
-import {IPoolAddressesProviderRegistry} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProviderRegistry.sol";
-import "forge-std/console.sol";
 
 contract UserVaultTest is BaseTestFork {
-    address poolDataProviderAddress = 0x2F7ae2EebE5Dd10BfB13f3fB2956C7b7FFD60A5F;
+    function _testSetup() internal override {}
 
     function testRepayDebt() public {
+        // chain data
+        uint256 MONAD_TESTNET_BLOCK_NUMBER = 30753577;
+        address user = 0x0000B06460777398083CB501793a4d6393900000;
+        uint256 userDebtUSDTWei = 8500000;
+        address USDT = 0x88b8E2161DEDC77EF4ab7585569D2415a1C1055D;
+        address variableDebtUSDT = 0x20838Ac96e96049C844f714B58aaa0cb84414d60;
+
+        // fork
+        //        uint256 monadTestnetFork = vm.createFork(MONAD_TESTNET_RPC_URL, MONAD_TESTNET_BLOCK_NUMBER);
+        //        vm.selectFork(monadTestnetFork);
+
         // arrange
-        IPoolAddressesProviderRegistry papr = IPoolAddressesProviderRegistry(poolDataProviderAddress);
-        address[] memory addresses = papr.getAddressesProvidersList();
 
-        address pool1 = addresses[0];
+        // act
 
-        emit log_named_address("pool address", pool1);
+        //emit log_named_address("pool address", pool1);
     }
 }
